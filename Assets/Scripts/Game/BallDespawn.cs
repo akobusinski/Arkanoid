36832e5ccbe66d27616e2b39bbdfadf128566cf3
GacekKosmatek <60139174+GacekKosmatek@users.noreply.github.com>
2022-09-25 13:57:48 +0200

    Remove debug log

--- Assets/Scripts/Game/BallDespawn.cs
+++ Assets/Scripts/Game/BallDespawn.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class BallDespawn : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D collision) {
-        Debug.Log("Collision");
         Destroy(collision.gameObject);
     }
 }
